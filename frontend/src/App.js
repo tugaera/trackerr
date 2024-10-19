f0113e6aaa9bcb648ed71543f9e5f59d39ddb4b9
tugaera <15148322+tugaera@users.noreply.github.com>
2024-10-19 15:03:43 +0100

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Settings from './Settings';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trackerr Settings</h1>
+      <Settings />
+    </div>
+  );
+}
+
+export default App;
